5cf1fc4a2f2c18223407fb99fa3cb3baedf84769
JoaoMachado <jvmachadorj@gmail.com>
2019-05-15 05:07:07 -0300

    Make app production ready

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geolocation_api_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geolocation_api_project.base.py')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
