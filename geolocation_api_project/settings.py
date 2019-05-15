f7ab13a54776a290afa91cb92f6e56d4badf77ab
JoaoMachado <jvmachadorj@gmail.com>
2019-05-15 04:58:47 -0300

    Create .env to enviroment variables

--- geolocation_api_project/settings.py
+++ geolocation_api_project/settings.py
@@ -13,13 +13,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3@9u%%b8k292evq!27a!ka_4(t+z1dgpu_gh@+ara7*o913e%u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
